76b8f48f4ae5038b18e691e4c5938ba545d5e00e
Dimitris Argyros <dimitrislargyros@gmail.com>
2026-06-24 23:17:18 +0300

    Unorthodox Athletes first commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ClientApp from './ClientApp.jsx'
+import TrainerApp from './TrainerApp.jsx'
+import './index.css'
+
+const isTrainer = window.location.pathname.startsWith('/trainer')
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  React.createElement(React.StrictMode, null,
+    isTrainer ? React.createElement(TrainerApp) : React.createElement(ClientApp)
+  )
+)
